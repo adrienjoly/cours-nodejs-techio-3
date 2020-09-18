b809599eeef652c7a552e56b7326eebc8f2c67a2
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-18 16:53:13 +0200

    test que le serveur répond, après l'avoir lancé dans un child_process

--- nodejs-project/server.spec.js
+++ nodejs-project/server.spec.js
@@ -1,16 +1,28 @@
-﻿const expect = require('expect.js');
+﻿const childProcess = require('child_process');
+const expect = require('expect.js');
 const fetch = require('node-fetch');
 const { printMessage } = require('./common/techio');
 
 
 describe('le serveur devrait', () => {
 
+  let server;
+
+  after(() => {
+    try {
+      server.kill();
+    } catch(err) {}
+  });
+
   it(`s'exécuter sans erreur`, () => {
-    require('./server.js'); // load and run student code
+    // load and run student code
+    // require('./server.js');
+    server = childProcess.fork('./server.js');
   });
 
   it(`accepter une requête HTTP GET à la racine`, async function() {
     this.retries(3);
+    await new Promise(resolve => setTimeout(resolve, 1000)); // wait one (more) second
     await fetch('http://localhost:3000/')
   })
 
