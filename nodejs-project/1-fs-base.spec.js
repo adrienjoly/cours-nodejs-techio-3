929a1d56dfe8aa18e3c7785dd78e521d1e375451
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-16 14:50:08 +0100

    exerciseur 1 terminé

--- nodejs-project/1-fs-base.spec.js
+++ nodejs-project/1-fs-base.spec.js
@@ -4,37 +4,58 @@ const {
   printMessage,
   getStubFile,
   getStudentCode,
+  runStudentCode,
 } = require("./common/techio");
 
 const CODE_FILE = process.env.CODE_FILE || getStubFile(__filename);
 
 describe("le programme devrait", () => {
-  it(`appeler la fonction readFile()`, async () => {
+  it(`appeler la fonction readFileSync()`, async () => {
     const studentCode = await getStudentCode(CODE_FILE);
-    expect(studentCode).to.match(/readFile\(/);
+    expect(studentCode).to.match(/readFileSync\(/);
   });
 
   it(`afficher le contenu d'un fichier ne contenant que des minuscules`, async () => {
-    const studentCode = await getStudentCode(CODE_FILE);
-    expect(studentCode).to.match(/readFile\(/);
+    const contenuAttendu = "bonjour le monde !";
+    const fichierSource = "minuscules.txt";
+    await fs.promises.writeFile(fichierSource, contenuAttendu);
+    const execution = await runStudentCode(CODE_FILE, { args: [fichierSource] });
+    const sortie = execution.getLogs().join("").trim();
+    expect(sortie).to.eql(contenuAttendu);
+  });
+
+  it(`afficher le contenu d'un fichier après conversion des majuscules en minuscules`, async () => {
+    const contenuInitial = "Bonjour le Monde !";
+    const contenuAttendu = contenuInitial.toLowerCase();
+    const fichierSource = "minuscules.txt";
+    await fs.promises.writeFile(fichierSource, contenuInitial);
+    const execution = await runStudentCode(CODE_FILE, { args: [fichierSource] });
+    const sortie = execution.getLogs().join("").trim();
+    expect(sortie).to.eql(contenuAttendu);
+  });
+
+  it(`écrire dans le fichier "résultat.txt" le contenu mis en minuscules`, async () => {
+    const contenuInitial = "Bonjour le Monde !";
+    const contenuAttendu = contenuInitial.toLowerCase();
+    const fichierSource = "minuscules.txt";
+    const fichierDestination = "résultat.txt";
+    await fs.promises.writeFile(fichierSource, contenuInitial);
+    await runStudentCode(CODE_FILE, { args: [fichierSource] });
+    const contenuDestination = await fs.promises.readFile(fichierDestination, "utf8");
+    expect(contenuDestination).to.eql(contenuAttendu);
   });
 
   /*
-  it(`afficher la réponse JSON dans la sortie standard, si une URL valide est fournie`, async () => {
-    const program = await runStudentCode(CODE_FILE, {
-      args: ["https://jsonplaceholder.typicode.com/photos/1"],
-    });
-    const response = program.getLogs().join("").trim();
-    expect(JSON.parse(response)).to.eql(EXPECTED_RESPONSE);
-  });
-
-  it(`afficher l'erreur dans la sortie d'erreurs, si une URL inexistante est fournie`, async () => {
-    const program = await runStudentCode(CODE_FILE, {
-      args: ["https://serveur-inexistant.xyz"],
-    });
+  it(`afficher l'erreur dans la sortie d'erreurs, en cas d'erreur de lecture de fichier`, async () => {
+    const fichierInexistant = "fichier-inexistant.txt";
+    const program = await runStudentCode(CODE_FILE, { args: [fichierInexistant] });
+    console.log('coucou')
     const error = program.getErrors().join("").trim();
-    expect(error).to.match(/^FetchError: /);
-    expect(error).to.match(/ENOTFOUND/);
+    console.log({error})
+    expect(error).to.match(/Error/);
+    expect(error).to.match(/ENOENT/);
+    expect(error).to.match(/no such file or directory/);
+    expect(error).to.match(fichierInexistant);
   });
   */
 
