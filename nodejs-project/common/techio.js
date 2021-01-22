f738b7072567cf937bf8f03cece1a6f23f1ce175
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-01-22 12:09:13 +0100

    fix: plus de logs pour l'étudiant, moins pour la CI

--- nodejs-project/common/techio.js
+++ nodejs-project/common/techio.js
@@ -1,8 +1,21 @@
 const fs = require('fs');
 const childProcess = require("child_process");
 
-exports.printMessage = (message, channel = "Exercice terminé") =>
-  console.log('\nTECHIO> message --channel "' + channel + '" "' + message + '"');
+const showStdOutputs = !process.env.HIDE_TECHIO_MESSAGES;
+
+exports.printMessage = (message, channel = "Finalisation de l'exercice") =>
+  process.env.HIDE_TECHIO_MESSAGES
+    ? {}
+    : console.log(`TECHIO> message --channel "${channel}" "${message}"`);
+
+exports.congratulateStudent = () => {
+  exports.printMessage(`🏆  Bravo, ton programme a l'air de fonctionner correctement et de respecter les consignes de l'exercice !`);
+  exports.printMessage(``);
+  exports.printMessage(`Avant de passer à l'exercice suivant, pense à:`);
+  exports.printMessage(`1. 🧹  nettoyer ton code, pour ne garder que les instructions et commentaires nécessaires à son bon fonctionnement;`);
+  exports.printMessage(`2. 📝  ajouter des commentaires aux parties du code les moins évidentes, pour te souvenir à quoi elles servent;`);
+  exports.printMessage(`3. 📒  puis à garder une copie du code dans tes notes de cours, de manière à les retrouver rapidement quand tu en auras besoin.`);
+};
 
 exports.getStubFile = (testFile) => './' + testFile.split('/').pop().replace('.spec.js', '.js');
 
--- test-all.sh
+++ test-all.sh
@@ -2,9 +2,16 @@
 
 set -e # stop script on any non-zero exit code
 
+function test {
+  # Hide TECHIO messages to students, from printMessage()
+  HIDE_TECHIO_MESSAGES=1 \
+  CODE_FILE="$1.solution.js" \
+  node_modules/mocha/bin/mocha "$1.spec.js"
+}
+
 cd nodejs-project
-CODE_FILE="1-fs-base.solution.js" node_modules/mocha/bin/mocha 1-fs-base.spec.js
-CODE_FILE="2-fs-async.solution.js" node_modules/mocha/bin/mocha 2-fs-async.spec.js
-CODE_FILE="3-fs-interm.solution.js" node_modules/mocha/bin/mocha 3-fs-interm.spec.js
-CODE_FILE="4-fs-promise.solution.js" node_modules/mocha/bin/mocha 4-fs-promise.spec.js
-CODE_FILE="5-fs-await.solution.js" node_modules/mocha/bin/mocha 5-fs-await.spec.js
+test "1-fs-base"
+test "2-fs-async"
+test "3-fs-interm"
+test "4-fs-promise"
+test "5-fs-await"
